cf1d96603e54136e4b445b275d5741fc95e41534
frankowskimarcin <marcinfrankowski11@gmail.com>
2021-05-10 01:32:22 +0200

    calculateEffectiveness

--- main.py
+++ main.py
@@ -4,18 +4,14 @@ from skimage.filters import frangi
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+from skimage.morphology import erosion
+from sklearn.metrics import confusion_matrix
 
 
-# image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-def resizeInputImage(dataDir, fileName, fovMaskDir, fovMaskFile):
+def resizeInputImage(dataDir, fileName):
     image = Image.open(dataDir + fileName)
     image = image.resize((1280, 853))
     image.save(dataDir + fileName[:-4] + "_small" + fileName[-4:])
-
-    fovImage = Image.open(fovMaskDir + fovMaskFile)
-    fovImage = fovImage.resize((1280, 853))
-    fovImage.save(fovMaskDir + fovMaskFile[:-4] + "_small" + fovMaskFile[-4:])
 
 
 def frangiFilter(image, fovImage):
@@ -32,30 +28,58 @@ def frangiFilter(image, fovImage):
     return image
 
 
+def calculateEffectiveness(image, expertImage):
+    image = image.reshape(-1)
+    expertImage = expertImage.reshape(-1)
+    cm = confusion_matrix(expertImage, image)
+
+    # true positive, false positive, false negative, true negative
+    tp, fp, fn, tn = cm[0][0], cm[0][1], cm[1][0], cm[1][1]
+    print("TP: {}, FP: {}, FN: {}, TN:{}".format(tp, fp, fn, tn))
+
+    acc = (tp + tn) / (tp + tn + fp + fn)               # accuracy
+    sens = tp / (tp + fn)                               # sensitivity
+    spec = tn / (tn + fp) if tn + fp != 0 else 0    # specificity
+    print("Accuracy: {}, Sensitivity: {}, Specificity: {}".format(acc, sens, spec))
+
+
 def main():
     dataDir = "data/healthy/"
     fileName = "02_h.jpg"
     fovMaskDir = "data/healthy_fovmask/"
-    fovMaskFile = '02_h_mask.tif'
+    fovMaskFile = fileName[:-4] + "_mask.tif"
+    expertMaskDir = "data/healthy_manualsegm/"
+    expertMaskFile = fileName[:-4] + ".tif"
     resize = True
 
     if resize:
-        resizeInputImage(dataDir, fileName, fovMaskDir, fovMaskFile)
+        resizeInputImage(dataDir, fileName)
+        resizeInputImage(fovMaskDir, fovMaskFile)
+        resizeInputImage(expertMaskDir, expertMaskFile)
         fileName = fileName[:-4] + "_small" + fileName[-4:]
         fovMaskFile = fovMaskFile[:-4] + "_small" + fovMaskFile[-4:]
+        expertMaskFile = expertMaskFile[:-4] + "_small" + expertMaskFile[-4:]
         print(fileName)
-        print(fovMaskFile)
 
     image = io.imread(dataDir + fileName)
     image = image[:, :, 1]
     fovImage = plt.imread(fovMaskDir + fovMaskFile)
     fovImage = fovImage[:, :, 1]
+    expertImage = plt.imread(expertMaskDir + expertMaskFile)
+    expertImage = expertImage.astype(np.uint8)
+    # expertImage = expertImage[:, :, 1]
+    # expertImage = cv2.cvtColor(expertImage, cv2.COLOR_GRAY2BGR)
+    # expertImage = cv2.cvtColor(expertImage, cv2.COLOR_BGR2GRAY)
 
     frangiImage = frangiFilter(image, fovImage)
+    frangiImage = erosion(frangiImage)
+    frangiImage = frangiImage.astype(np.uint8)
 
-    io.imshow(frangiImage)
-    io.show()
-    io.imsave('data/output/out.jpg', frangiImage)
+    calculateEffectiveness(frangiImage, expertImage)
+
+    # io.imshow(frangiImage)
+    # io.show()
+    # io.imsave('data/output/out.jpg', frangiImage)
 
 
 if __name__ == '__main__':
